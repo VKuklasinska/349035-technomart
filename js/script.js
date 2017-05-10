4932da6c7a90b495cf457628b8f3f6b9390a7033
ViktoriiaZybina <ViktoriiaZybina@users.noreply.github.com>
2017-05-10 23:39:17 +0200

    JS improved and little details checked on CSS and HTML

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@
       var loginStorage = localStorage.getItem("name");
       var mailStorage = localStorage.getItem("mail");
       var message = popup.querySelector("textarea");
+      var overlay = document.querySelector(".modal-overlay");
 
       link.addEventListener("click", function(event) {
         event.preventDefault();
@@ -26,12 +27,14 @@
         if (!loginStorage && !mailStorage) {
           login.focus();
         }
+        overlay.classList.add("modal-overlay-show");
       });
 
       close.addEventListener("click", function(event) {
         event.preventDefault();
         popup.classList.remove("modal-content-show");
         popup.classList.remove("modal-error");
+        overlay.classList.remove("modal-overlay-show");
       });
 
       form.addEventListener("submit", function(event) {
@@ -49,6 +52,7 @@
           if (popup.classList.contains("modal-content-show")) {
             popup.classList.remove("modal-content-show");
             popup.classList.remove("modal-error");
+            overlay.classList.remove("modal-overlay-show");
           }
         }
       });
@@ -58,45 +62,60 @@
       var mapOpen = document.querySelector(".js-open-map");
       var mapPopup = document.querySelector(".modal-content-map");
       var mapClose = mapPopup.querySelector(".close-btn");
+      var mapOverlay = document.querySelector(".modal-overlay");
 
       mapOpen.addEventListener("click", function(event) {
         event.preventDefault();
         mapPopup.classList.add("modal-content-show");
+        mapOverlay.classList.add("modal-overlay-show");
       });
 
       mapClose.addEventListener("click", function(event) {
         event.preventDefault();
         mapPopup.classList.remove("modal-content-show");
+        mapOverlay.classList.remove("modal-overlay-show");
       });
+
+      // mapOverlay.addEventListener("click", function(event) {
+      //   event.preventDefault();
+      //   mapPopup.classList.remove("modal-content-show");
+      //   mapOverlay.classList.remove("modal-overlay-show");
+      // });
 
       window.addEventListener("keydown", function(event) {
         if (event.keyCode === 27) {
             if (mapPopup.classList.contains("modal-content-show")) {
             mapPopup.classList.remove("modal-content-show");
+            mapOverlay.classList.remove("modal-overlay-show");
           }
         }
       });
 
 // Для корзины
 
-      var cartOpen = document.querySelector(".bookmark");
+      var cartOpenList = document.querySelectorAll(".bookmark");
       var cartPopup = document.querySelector(".modal-content-cart");
       var cartClose = cartPopup.querySelector(".close-btn");
 
-      cartOpen.addEventListener("click", function(event) {
-        event.preventDefault();
-        cartPopup.classList.add("modal-content-show");
-      });
+      for (var iList = 0; iList < cartOpenList.length; iList++) {
+        cartOpenList[iList].addEventListener("click", function(event) {
+          event.preventDefault();
+          cartPopup.classList.add("modal-content-show");
+          overlay.classList.add("modal-overlay-show");
+        });
+      }
 
       cartClose.addEventListener("click", function(event) {
         event.preventDefault();
         cartPopup.classList.remove("modal-content-show");
+        overlay.classList.remove("modal-overlay-show");
       });
 
       window.addEventListener("keydown", function(event) {
         if (event.keyCode === 27) {
             if (cartPopup.classList.contains("modal-content-show")) {
             cartPopup.classList.remove("modal-content-show");
+            overlay.classList.remove("modal-overlay-show");
           }
         }
       });
