9d837783c82bc4f2fce0d0a1f6a9df7eabfc1f2e
ViktoriiaZybina <ViktoriiaZybina@users.noreply.github.com>
2017-05-11 00:23:19 +0200

    js split on 2 parts - for index.html and catalog.html

--- js/script.js
+++ js/script.js
@@ -90,32 +90,3 @@
           }
         }
       });
-
-// Для корзины
-
-      var cartOpenList = document.querySelectorAll(".bookmark");
-      var cartPopup = document.querySelector(".modal-content-cart");
-      var cartClose = cartPopup.querySelector(".close-btn");
-
-      for (var iList = 0; iList < cartOpenList.length; iList++) {
-        cartOpenList[iList].addEventListener("click", function(event) {
-          event.preventDefault();
-          cartPopup.classList.add("modal-content-show");
-          overlay.classList.add("modal-overlay-show");
-        });
-      }
-
-      cartClose.addEventListener("click", function(event) {
-        event.preventDefault();
-        cartPopup.classList.remove("modal-content-show");
-        overlay.classList.remove("modal-overlay-show");
-      });
-
-      window.addEventListener("keydown", function(event) {
-        if (event.keyCode === 27) {
-            if (cartPopup.classList.contains("modal-content-show")) {
-            cartPopup.classList.remove("modal-content-show");
-            overlay.classList.remove("modal-overlay-show");
-          }
-        }
-      });
